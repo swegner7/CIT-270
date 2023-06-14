304537070fb1f5953cf9bdd3f2bfc647162dd858
Spencer Wegner <swegner@byui.edu>
2023-06-14 13:07:48 -0600

    W09 - chain to CA

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json()); //allow json requests | JSON(Javascript Object Notat
 https.createServer({
     key: fs.readFileSync('/etc/letsencrypt/archive/spencerwegner.cit270.com/privkey1.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/archive/spencerwegner.cit270.com/cert1.pem'),
-    chain: fs.readFileSync('/etc/letsencrypt/archive/spencerwegner.cit270.com/fullchain1.pem')
+    ca: fs.readFileSync('/etc/letsencrypt/archive/spencerwegner.cit270.com/chain1.pem')
   },
    app).listen(port, () => {
     redisClient.connect();
