3f7372d6dc2159e4c6c7347615c0f3edb7e661fc
swegner7 <swegner@byui.edu>
2023-07-17 13:23:00 -0600

    yay

--- login.cmd
+++ login.cmd
@@ -1,5 +1,5 @@
 echo "Logging in"
 
-curl --insecure -v -d "@login.json" POST -H "Content-Type:application/json" http://localhost:3000/login 
+curl --insecure -v -d "@login.json" POST -H "Content-Type:application/json" https://spencerwegner.cit270.com/login 
 
 @REM curl -v https://dev.stedi.me/validate/5bb368ac-7ed6-4c4b-bc77-3a8fc5e25044
